fb3d24798f03ee89097e46dd3fea2938efc8803a
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-16 15:52:58 +0800

    Explored data types promotion
    Started string understanding.

--- string_expo.v
+++ string_expo.v
@@ -3,4 +3,6 @@ fn main()
 	name := "Alexander"
 	nickname := name[0..4]
 	println("Hello, ${name}. Can I call you ${nickname} for short?")
+
+	
 }
--- variables.v
+++ variables.v
@@ -22,4 +22,20 @@ fn main()
 	println("n1 = ${n1}, n2 = ${n2}")
 	// This is nice like in Python's one-line swapping.
 
+	u := u16(12)
+	println(u)
+	v := 13 + u    // v is of type `u16` - no promotion
+	println(v)
+	x := f32(45.6)
+	println(x)
+	y := x + 3.14  // y is of type `f32` - no promotion
+	println(y)
+	a := 75        // a is of type `int` - default for int literal
+	println(a)
+	b := 14.7      // b is of type `f64` - default for float literal
+	println(b)
+	c := u + a     // c is of type `int` - automatic promotion of `u`'s value
+	println(c)
+	d := b + x     // d is of type `f64` - automatic promotion of `x`'s value
+	println(d)
 }
