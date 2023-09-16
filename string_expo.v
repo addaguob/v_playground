57e53695cd880c77f9710937f79fe6ed913d1922
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-16 20:25:23 +0800

    Explore and add strings

--- string_expo.v
+++ string_expo.v
@@ -1,8 +1,11 @@
 fn main()
 {
-	name := "Alexander"
+	name := "Alexander Daguob"
 	nickname := name[0..4]
 	println("Hello, ${name}. Can I call you ${nickname} for short?")
-
-	
+	initials := name[0].ascii_str() + name[10].ascii_str()
+	println("Your initials are ${initials}.")
+	println(name.len)	
+	star_str := '\u2605'
+	println(star_str)
 }
