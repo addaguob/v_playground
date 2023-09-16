9ec31de44dee79ce399e54a1bfdcc792cbb1c275
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-16 10:09:59 +0800

    Exploring functions that cann add, subtract, multiply and divide

---
+++ funcs.v
@@ -0,0 +1,28 @@
+fn main()
+{
+	println(add(1,2))
+	println(sub(5,2))
+	println(mult(2,2))
+	println(divd(4,2))
+
+}
+
+fn add(x int, y int) int 
+{
+	return x + y
+}
+
+fn sub(x int, y int) int 
+{
+	return x - y
+}
+
+fn mult(x int, y int) int 
+{
+	return x * y
+}
+
+fn divd(x int, y int) int 
+{
+	return x / y
+}
