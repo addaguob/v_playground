c879c8a2357038f73e747a9e25b14c06a2c560d9
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-17 20:17:40 +0800

    String operators expo

--- string_expo.v
+++ string_expo.v
@@ -28,4 +28,24 @@ fn main()
 	println(n8)
 	println(n3850)
 	println(neg3850)
+
+	x := 123.4567
+	println('[${x:.2}]') // round to two decimal places => [123.46]
+	println('[${x:10}]') // right-align with spaces on the left => [   123.457]
+	println('[${int(x):-10}]') // left-align with spaces on the right => [123       ]
+	println('[${int(x):010}]') // pad with zeros on the left => [0000000123]
+	println('[${int(x):b}]') // output as binary => [1111011]
+	println('[${int(x):o}]') // output as octal => [173]
+	println('[${int(x):X}]') // output as uppercase hex => [7B]
+
+	println('[${10.0000:.2}]') // remove insignificant 0s at the end => [10]
+	println('[${10.14300:.2}]') // remove insignificant 0s at the end => [10]
+	println('[${10.0000:.2f}]') // do show the 0s at the end, even though they do not change the number => [10.00]
+
+	name2 := 'Bob'
+	bobby := name2 + 'by' // + is used to concatenate strings
+	println(bobby) // "Bobby"
+	mut greeting := 'hello '
+	greeting += 'world' // `+=` is used to append to a string
+	println(greeting) // "hello world"
 }
