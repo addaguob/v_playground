88dc2407147315efadd90870b7a7f5a817de45f5
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-17 22:16:30 +0800

    Hexadecimal, binary or octal notation

--- rune_numbers.v
+++ rune_numbers.v
@@ -7,9 +7,27 @@ println(hello)
 hello_runes := hello.runes() // [`H`, `e`, `l`, `l`, `o`, ` `, `W`, `o`, `r`, `l`, `d`, ` `, `👋`]
 println(hello_runes.string() == hello)
 
-a := 0x7B
+mut a := i64(0x7B)
 println(a)
-b := 0b01111011
+mut b := 0b01111011
 println(b)
-c := 0o173
+mut c := 0o173
+println(c)
+
+num := 1_000_000 // same as 1000000
+println(num)
+three := 0b0_11 // same as 0b11
+println(three)
+float_num := 3_122.55 // same as 3122.55
+println(float_num)
+hexa := 0xF_F // same as 255
+println(hexa)
+oct := 0o17_3 // same as 0o173
+println(oct)
+
+a = i64(123)
+println(a)
+b = u8(42)
+println(b)
+c = i16(12345)
 println(c)
