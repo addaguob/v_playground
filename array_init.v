caec7e12c1ec0bdd758629c1cab4f710071a4ac4
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-29 00:31:29 +0800

    Explore array with index number as value

--- array_init.v
+++ array_init.v
@@ -1,8 +1,17 @@
 fn main()
 {
 	arr := []int{len: 5, cap: 3, init: 1}
-	println(arr.capm, arr.len)
-	for i in arr{
+	println(arr.cap)
+	println(arr.len)
+
+	for i in arr {
 		print(i.str() + " ")
+	}
+
+	println("")
+
+	arr_ind := [] int {len: 10, init: index}
+	for i in arr_ind {
+		println(i)
 	}
 }
