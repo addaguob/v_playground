3a049cdf3b77ece688cc60a41b419d889c286e20
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-29 21:43:47 +0800

    Exlpore triple-dimensional arrays

--- mult_arrs.v
+++ mult_arrs.v
@@ -1,4 +1,18 @@
-mut arr := [][]int{len: 3, init: []int{len:3, init: 1}}
-println(arr)
-arr[0][1] = 5
-println(arr)
+mut arr2 := [][]int{len: 3, init: []int{len:3, init: 1}}
+println(arr2)
+arr2[0][1] = 5
+println(arr2)
+
+mut arr3 := [][][]int{
+	len: 3,
+	init: [][]int{
+		len: 3,
+		init: []int{
+			len: 3,
+			init: 1
+		}
+	}
+}
+println(arr3)
+arr3[2][2][2] = 111
+println(arr3)
