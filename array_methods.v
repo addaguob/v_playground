51a5aef2242a38bc0069ceb6e4451ecc21570adc
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-01 00:03:26 +0800

    Explore filter with anonymous functionw

--- array_methods.v
+++ array_methods.v
@@ -3,4 +3,8 @@ fn main()
 	nums := [1, 2, 3, 4, 5, 6, 7, 8]
 	even := nums.filter(it % 2 == 0)
 	println(even)
+
+	// Filter with anonymous function
+	even_fn := nums.filter(fn (x int) bool { return x % 2 == 0 })
+	println(even_fn)
 }
