454f876a543361312d9163f5f14a1e221c342abc
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-04 21:28:19 +0800

    Sort the array ascending on name and descending age

---
+++ custom_sort.v
@@ -0,0 +1,31 @@
+// sorts the array ascending on name and descending age
+
+struct User {
+	age  int
+	name string
+}
+
+mut users := [User{21, 'Bob'}, User{65, 'Bob'}, User{25, 'Alice'}]
+
+custom_sort_fn := fn (a &User, b &User) int {
+	// return -1 when a comes before b
+	// return 0, when both are in same order
+	// return 1 when b comes before a
+	if a.name == b.name {
+		if a.age < b.age {
+			return 1
+		}
+		if a.age > b.age {
+			return -1
+		}
+		return 0
+	}
+	if a.name < b.name {
+		return -1
+	} else if a.name > b.name {
+		return 1
+	}
+	return 0
+}
+users.sort_with_compare(custom_sort_fn)
+println(users)
