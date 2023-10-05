e8ff368edbebf3cf389a7ef87aa265aa5f5ce117
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-05 22:28:08 +0800

    No left or right indices

--- array_slices.v
+++ array_slices.v
@@ -3,10 +3,14 @@ mut arr2 := [5, 7]
 arr2 << arr1
 println(arr2)
 
-// Try 2 values from right
-arr2 << arr1[3..]
-println(arr2)
-
 // Try clearing the mutable array
 arr2 = []
 println(arr2)
+
+// No right index 
+arr2 << arr1[3..]
+println(arr2) // ... is index:len by default
+
+// No left index
+arr2 = arr1[..3]
+println(arr2) // ...is index:0 by default
