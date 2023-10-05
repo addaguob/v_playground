a29a41ab5c31ee2af740691993a63ce73161d067
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-05 22:22:15 +0800

    Update array_slices.v

--- array_slices.v
+++ array_slices.v
@@ -2,3 +2,11 @@ arr1 := [1, 4, 3, 6, 9, 2]
 mut arr2 := [5, 7]
 arr2 << arr1
 println(arr2)
+
+// Try 2 values from right
+arr2 << arr1[3..]
+println(arr2)
+
+// Try clearing the mutable array
+arr2 = []
+println(arr2)
