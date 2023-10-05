cfae77d4f27e3760342bb0ddcf6f1fce022c2d1d
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-05 22:40:18 +0800

    Reference reallocation between two arrays

--- array_slices.v
+++ array_slices.v
@@ -18,3 +18,12 @@ println(arr2) // ...is index:0 by default
 // 2nd and 4th indices
 arr2 = arr1[1..3]
 println(arr2)
+
+mut a := [0, 1, 2, 3, 4, 5]
+mut b := a[2..4]
+b[0] = 7 // `b[0]` is referring to `a[2]`
+println(a) // `[0, 1, 7, 3, 4, 5]`
+b << 9
+// `b` has been reallocated and is now independent from `a`
+println(a) // `[0, 1, 7, 3, 4, 5]` - no change
+println(b) // `[7, 3, 9]`
