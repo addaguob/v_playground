c763f8459db04d555394fdadf5f689044205cafb
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2023-10-10 14:24:20 +0800

    Test fixed array size

--- arrays.v
+++ arrays.v
@@ -38,3 +38,17 @@ println(numbers.len) // 0
 for i in 0 .. 1000 {
 	numbers << i
 }
+
+
+mut fnums := [3]int{} // fnums is a fixed size array with 3 elements.
+fnums[0] = 1
+fnums[1] = 10
+fnums[2] = 100
+println(fnums) // => [1, 10, 100]
+println(typeof(fnums).name) // => [3]int
+
+fnums2 := [1, 10, 100]! // short init syntax that does the same (the syntax will probably change)
+
+anums := fnums[..] // same as `anums := fnums[0..fnums.len]`
+println(anums) // => [1, 10, 100]
+println(typeof(anums).name) // => []int
