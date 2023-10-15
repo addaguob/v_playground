7f9c17e9b3736b98d66cd9f138fc487cc383c506
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-15 23:52:58 +0800

    Try with maps

---
+++ maps.v
@@ -0,0 +1,8 @@
+mut m := map[string]int{} // a map with `string` keys and `int` values
+m['one'] = 1
+m['two'] = 2
+println(m['one']) // "1"
+println(m['bad_key']) // "0"
+println('bad_key' in m) // Use `in` to detect whether such key exists
+println(m.keys()) // ['one', 'two']
+m.delete('two')
